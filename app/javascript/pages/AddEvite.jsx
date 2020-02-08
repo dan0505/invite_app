32647381931dc6ba45fa31dbe5f607679d3f1e61
danielq <qiaoer0505@gmail.com>
2020-02-08 17:05:45 +1100

    fix syntax error, add link to menu

--- app/javascript/pages/AddEvite.jsx
+++ app/javascript/pages/AddEvite.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Layout, Menu, Icon, Skeleton, Button, Popover, Steps, Row, Col } from "antd";
+import { Link } from 'react-router-dom';
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 const { Step } = Steps;
@@ -58,8 +59,8 @@ export default class AddEvent extends React.Component {
             defaultSelectedKeys={["2"]}
             style={{ lineHeight: "64px" }}
           >
-            <Menu.Item key="1">Updates</Menu.Item>
-            <Menu.Item key="2"><Icon type="plus" />New Evite</Menu.Item>
+            <Menu.Item key="1"><Link to="/updates" className="nav-text">Updates</Link></Menu.Item>
+            <Menu.Item key="2"><Link to="/add-evite" className="nav-text"><Icon type="plus" />New Evite</Link></Menu.Item>
             <Menu.Item key="3">Groups</Menu.Item>
           </Menu>
           <div style={{ position: "absolute", top: "0", right: "15px" }}>
